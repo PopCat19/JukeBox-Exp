115e6db7d8fb5200c8ee01a5e8725d75e37bd4e2
PopCat19 <atsuo11111@gmail.com>
2026-03-22 19:19:22 -0400

    chore: remove orphaned PlayerAnimator (never wired into SongEditor)

--- editor/core/index.ts
+++ editor/core/index.ts
@@ -14,4 +14,3 @@ export { KeyboardHandler } from "./keyboard-handler";
 export { ChangeDispatcher } from "./change-dispatcher";
 export { PromptManager } from "./prompt-manager";
 export { ModSliderRegistry } from "./mod-slider-registry";
-export { PlayerAnimator } from "./player-animator";
