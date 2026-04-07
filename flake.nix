43debb273e408c605343f5fffa8b8e941a57f2ac
PopCat19 <atsuo11111@gmail.com>
2026-04-07 13:50:42 -0400

    chore: switch nixpkgs to unstable, update biome schema to 2.4.10

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "JukeBox-Exp dev environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs =
