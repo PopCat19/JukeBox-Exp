1dd7fac5db52f91f5082731fa0a1c87f95abbd7e
PopCat19 <atsuo11111@gmail.com>
2026-06-19 05:28:02 -0400

    fix(spectrum): FG 300-6000Hz

--- shared/spectrum.ts
+++ shared/spectrum.ts
@@ -14,7 +14,7 @@ import { events } from "./events";
 
 const FG_BANDS = 16;
 const FG_MIN_FREQ = 300;
-const FG_MAX_FREQ = 8000;
+const FG_MAX_FREQ = 6000;
 
 const BG_BANDS = 12;
 const BG_MIN_FREQ = 30;
