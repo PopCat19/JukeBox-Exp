c2dfad9cbc8841472330b6dd99c16138d945d310
PopCat19 <atsuo11111@gmail.com>
2026-06-22 13:08:50 -0400

    fix(cvv): bump popup spectrum height to 96px

--- editor/prompts/channel-volume-visualizer-prompt.ts
+++ editor/prompts/channel-volume-visualizer-prompt.ts
@@ -242,8 +242,8 @@ export class ChannelVolumeVisualizerPrompt extends BasePrompt {
 	private readonly _cvSpectrum: spectrumCanvas = new spectrumCanvas(
 		canvas({
 			width: 384,
-			height: 72,
-			style: "display: none; position: absolute; bottom: 0; left: 0; width: 100%; height: 72px; pointer-events: none; z-index: 1;",
+			height: 96,
+			style: "display: none; position: absolute; bottom: 0; left: 0; width: 100%; height: 96px; pointer-events: none; z-index: 1;",
 		}),
 		1,
 		true,
